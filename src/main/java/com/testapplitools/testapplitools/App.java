70ff04c469bc7357be825881b66b58dc91143b65
manikantp <manikantp497@gmail.com>
2019-11-27 10:25:43 +0530

    Commit

---
+++ src/main/java/com/testapplitools/testapplitools/App.java
@@ -0,0 +1,13 @@
+package com.testapplitools.testapplitools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
